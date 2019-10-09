0dc82db6af4efebfdb784dd98e83697a5490d1bc
jonath <jonath@jonathdeMacBook-Pro.local>
2019-10-09 16:01:09 +0800

    :memo:添加上传注释

--- router/upload.js
+++ router/upload.js
@@ -3,11 +3,15 @@ const fs = require("fs")
 const route = require("koa-router")()
 
 route.post("/", ctx => {
+  // 上传的文件
   const files = ctx.request.files
-  const paths = {}
+  const body = {}
+  // 上传文件地址
   const staticPath = path.join(__dirname, "../static/uploads/")
   Object.keys(files).forEach(key => {
+    // 上传的文件
     const file = files[key]
+    // 修改文件名称
     const oldPath = staticPath + path.basename(file.path)
     const newPath = staticPath + file.name
     fs.rename(
@@ -15,9 +19,10 @@ route.post("/", ctx => {
       newPath,
       error => error && console.log(error, "文件名修改失败...")
     )
-    paths[key] = ctx.origin + "/uploads/" + file.name
+    // 查看文件路径
+    body[key] = ctx.origin + "/uploads/" + file.name
   })
-  ctx.body = paths
+  ctx.body = body
 })
 
 module.exports = route
